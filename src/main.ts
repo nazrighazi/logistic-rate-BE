837fa189e8dac528e1070b850b3b8174ecc46c87
nazri ghazi <nazri.ghazi@time.com.my>
2025-08-27 22:57:14 +0800

    feat(docs): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Declare swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Logistic Rate Documentation')
+    .setDescription('The Logistic Rate API description')
+    .setVersion('1.0')
+    .addTag('logistic-rate')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
